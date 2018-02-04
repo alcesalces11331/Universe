40f646133de98fbaf7e9dbe927e8f2c22293ec25
Rollin Metzger <rollin.metzger@gmail.com>
2018-02-04 11:47:49 -0600

    first commit

---
+++ universe.rb
@@ -0,0 +1,16 @@
+class Universe
+	# Here we will instantiate the universe. Welcome to the Matrix.
+	class Life
+		class Terrestrial 
+		end
+
+		class Extra_Terrestrial
+		end
+	end
+
+	class Planet
+	end
+
+	class Galaxy
+	end
+end
