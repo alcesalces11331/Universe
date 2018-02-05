d6eb54752c20281c0b7527351f668435e3fbde3b
alcesalces11331 <rollin.metzger@gmail.com>
2018-02-04 18:45:03 -0600

    added to initialize; career class method

--- universe.rb
+++ universe.rb
@@ -3,12 +3,17 @@ class Universe
 	class Life
 		class Terrestrial 
 			class Human
-				attr_reader :first_name, :last_name, :middle_name, :city, :birthday
+				attr_accessor :career
+				attr_reader :first_name, :last_name, :middle_name, :city, :birthday, :existential, :status, :sex, :gender, :sexual_orientation
 
 				@@total_humans = 0
 				@@existential_attitudes = []
+				@@cities = []
+				@@careers = []
+				@@
 
-				def initialize(first_name, last_name, middle_name, city, birthday, existential)
+
+				def initialize(first_name, last_name, middle_name, city, birthday, existential, sex, gender, sexual_orientation)
 					@@total_humans += 1
 					@first_name = first_name
 					@last_name = last_name
@@ -17,13 +22,23 @@ class Universe
 					@birthday = birthday
 					@existential = existential 
 					@@existential_attitudes << @existential
+					@sex = sex 
+					@gender = gender
+					@sexual_orientation = sexual_orientation
 				end
 
 				def existential_prediliction(existential)
 					@existential = existential
 				end
 
-				def status
+				def status(status)
+					@status = status
+				end
+
+				def career(career)
+					@career = career
+					@@career << career if !@@career.include?(career)
+				end
 			end
 		end
 
