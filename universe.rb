36497a92ad818c35239c3fe34ac3fbba213264de
alcesalces11331 <rollin.metzger@gmail.com>
2018-02-04 18:36:22 -0600

    added class methods to class Human

--- universe.rb
+++ universe.rb
@@ -3,6 +3,27 @@ class Universe
 	class Life
 		class Terrestrial 
 			class Human
+				attr_reader :first_name, :last_name, :middle_name, :city, :birthday
+
+				@@total_humans = 0
+				@@existential_attitudes = []
+
+				def initialize(first_name, last_name, middle_name, city, birthday, existential)
+					@@total_humans += 1
+					@first_name = first_name
+					@last_name = last_name
+					@middle_name = middle_name
+					@city = city
+					@birthday = birthday
+					@existential = existential 
+					@@existential_attitudes << @existential
+				end
+
+				def existential_prediliction(existential)
+					@existential = existential
+				end
+
+				def status
 			end
 		end
 
